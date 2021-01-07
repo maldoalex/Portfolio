4a501cdce47c202c4d8b149051ec35843c80ebe9
maldoalex <maldoalex@gmail.com>
2021-01-07 14:12:51 -0600

    Contact Form submission with emailJS

--- js/scripts.js
+++ js/scripts.js
@@ -54,3 +54,29 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
+
+    // Contact Form Control
+    const btn = document.getElementById('button');
+
+        document.getElementById('form')
+            .addEventListener('submit', function (event) {
+                event.preventDefault();
+
+                btn.value = 'Sending...';
+
+                const serviceID = 'default_service';
+                const templateID = 'template_8djy0vh';
+
+                emailjs.sendForm(serviceID, templateID, this)
+                    .then(() => {
+                        btn.value = 'Send Email';
+                        alert('Sent!');
+                        document.getElementById('firstName').value=''; 
+                        document.getElementById('lastName').value='';
+                        document.getElementById('email').value=''; 
+                        document.getElementById('message').value='';
+                    }, (err) => {
+                        btn.value = 'Send Email';
+                        alert(JSON.stringify(err));
+                    });
+            });
